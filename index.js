1e4845ec928b9f52dc7539fdfa685daa6386dc40
Noman Haider <152289775+nomanhaider99@users.noreply.github.com>
2024-03-20 16:51:17 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from 'chalk';
+while (true) {
+    let user = await inquirer.prompt([
+        { type: "number", name: "userInput", message: "Guess the number: " }
+    ]);
+    let computer = Math.floor(Math.random() * 3);
+    if (user.userInput === computer) {
+        console.log(`${chalk.cyanBright("Congratulations...")} You guessed the number`);
+        console.log(`Computer: ${computer}`);
+    }
+    else {
+        console.log(`${chalk.redBright("Try Again...")}`);
+        console.log(`Computer: ${computer}`);
+    }
+}
